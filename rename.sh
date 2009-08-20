c349cec902667fd07948d858104f5f5c79713103
Jakub Vysoky <jakub@borka.cz>
2009-08-20 11:06:42 +0200

    comments

--- rename.sh
+++ rename.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+# currdir
 C=$( cd $( dirname $0 ); pwd )
-
+# tmpdir
 D=$( mktemp -d )
+
+# exclude .git dir
 mv .git ${D}
 
+# do rename with all params
 ${C}/rename.py "$@"
 
+# return .git dir here
 mv ${D}/.git .
 rmdir ${D}
 
+# remove deleted files and add new ones
 git ls-files -z --deleted | git update-index -z --remove --stdin
 git add -A
 
-
+# write nice commit message
 git commit -F - <<EOF
 automatic rename via rename.py
 
