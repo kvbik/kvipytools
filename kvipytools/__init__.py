df6f4e91f0f41007b2761cd1b24ee52dbfe72ee4
Jakub Vysoky <jakub@borka.cz>
2010-03-08 10:45:04 +0100

    better version handling

---
+++ kvipytools/__init__.py
@@ -0,0 +1,5 @@
+
+VERSION = (0, 1, 2)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup
+from os import path
+
+
+VERSION = (0, 1, 2)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+f = open(path.join(path.dirname(__file__), 'README.rst'))
+long_description = f.read().strip()
+f.close()
+
 
 setup(
     name = 'KviPyTools',
     description = "kvbik's python tools",
-    url = "http://github.com/kvbik/python-scripts",
-    version = '0.1.1',
+    url = "http://github.com/kvbik/kvipytools",
+    long_description = long_description,
+    version = __versionstr__,
     author = "Jakub Vysoky",
     author_email = "jakub@borka.cz",
     license = "BSD",
